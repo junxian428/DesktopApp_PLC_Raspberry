bb899034de59862c681d0a36bd1a34852f0a995b
junxian428 <wengyin777@gmail.com>
2023-04-10 02:16:12 +0100

    update

--- Main.py
+++ Main.py
@@ -6,13 +6,18 @@ from PyQt5.QtCore import Qt
 from openpyxl import load_workbook
 from subprocess import call
 from tkinter import messagebox
+import serial
 
-global str_total
-global location
+
+#global str_total
+#global location
 class Ui(QtWidgets.QMainWindow):
     def __init__(self):
           super(Ui, self).__init__()
           uic.loadUi('UI/Main.ui', self) # Load the .ui file
+
+          self.setWindowTitle('PLC Raspberry Pi')
+        
           self.button = self.findChild(QtWidgets.QPushButton, 'pushButton')
           self.button.clicked.connect(self.helloWorld)
           #self.button = self.findChild(QtWidgets.QPushButton, 'area')
@@ -26,7 +31,18 @@ class Ui(QtWidgets.QMainWindow):
 
 
     def helloWorld(self):
-          print("Hello World")
+          # create a serial object with the port name and baud rate
+          # open the serial port
+          ser = serial.Serial('/dev/ttyUSB0', 9600, timeout=1)
+
+          # write the command to the serial port
+          ser.write(b'@10MS0004000346*')
+          # read the response from the PLC
+          response = ser.read(100)
+
+          print(response)
+    
+          ser.close()
 
 
 app = QtWidgets.QApplication(sys.argv)
